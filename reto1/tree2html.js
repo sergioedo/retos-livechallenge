e99de071f93b707d3e80b75a0da8eaead5f2b481
Sergio Edo <sergio.edo@gmail.com>
2021-02-13 00:10:12 +0100

    pasamos a espacios

--- reto1/tree2html.js
+++ reto1/tree2html.js
@@ -1,14 +1,14 @@
 const convertJsonToHtml = (tree) => {
-	const newLine = '\n'
-	const indent = '\t'
-	const baseTag = tree.tag
-	const childrenTag = 'li'
-	let html = `<${baseTag}>${newLine}`
-	tree.children.map(e => {
-		html += `${indent}<${childrenTag}>${e.text}</${childrenTag}>${newLine}`
-	})
-	html += `</${baseTag}>`
-	return html
+    const newLine = '\n'
+    const indent = '\t'
+    const baseTag = tree.tag
+    const childrenTag = 'li'
+    let html = `<${baseTag}>${newLine}`
+    tree.children.map(e => {
+        html += `${indent}<${childrenTag}>${e.text}</${childrenTag}>${newLine}`
+    })
+    html += `</${baseTag}>`
+    return html
 }
 
 module.exports = convertJsonToHtml;
