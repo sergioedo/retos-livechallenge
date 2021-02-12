e62f53bc0281ccfb507dcf9c7d65eb101dae2742
Sergio Edo <sergio.edo@gmail.com>
2021-02-13 00:11:44 +0100

    mas espacios

--- reto1/tree2html.test.js
+++ reto1/tree2html.test.js
@@ -3,26 +3,26 @@ const convertJsonToHtml = require('./tree2html')
 // Basic sample
 // Input:
 const tree = {
-	tag: "ul",
-	children: [
-		{
-			text: "Primer elemento"
-		},
-		{
-			text: "Cáspita, otro elemento"
-		},
-		{
-			text: "El último"
-		}
-	]
+    tag: "ul",
+    children: [
+        {
+            text: "Primer elemento"
+        },
+        {
+            text: "Cáspita, otro elemento"
+        },
+        {
+            text: "El último"
+        }
+    ]
 }
 // Output:
 const html = `<ul>
-	<li>Primer elemento</li>
-	<li>Cáspita, otro elemento</li>
-	<li>El último</li>
+    <li>Primer elemento</li>
+    <li>Cáspita, otro elemento</li>
+    <li>El último</li>
 </ul>`
 
 test('convert basic tree to HTML', () => {
-	expect(convertJsonToHtml(tree)).toBe(html);
+    expect(convertJsonToHtml(tree)).toBe(html);
 })
