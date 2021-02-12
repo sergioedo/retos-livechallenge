03878d74940cb8baac4e24d4e03dda48499d9933
Sergio Edo <sergio.edo@gmail.com>
2021-02-13 00:06:20 +0100

    caso basico pasa los tests

--- reto1/tree2html.js
+++ reto1/tree2html.js
@@ -1,6 +1,14 @@
-const convertJsonToHtml = () => {
-    // TODO: pendiente implementar
-    return ''
+const convertJsonToHtml = (tree) => {
+	const newLine = '\n'
+	const indent = '\t'
+	const baseTag = tree.tag
+	const childrenTag = 'li'
+	let html = `<${baseTag}>${newLine}`
+	tree.children.map(e => {
+		html += `${indent}<${childrenTag}>${e.text}</${childrenTag}>${newLine}`
+	})
+	html += `</${baseTag}>`
+	return html
 }
 
 module.exports = convertJsonToHtml;
