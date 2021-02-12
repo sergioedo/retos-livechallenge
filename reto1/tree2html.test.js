39747f1d24c3d67ba956223ee3c4e4b8036e9901
Sergio Edo <sergio.edo@gmail.com>
2021-02-13 00:43:23 +0100

    reto 1 extra 2 (test)

--- reto1/tree2html.test.js
+++ reto1/tree2html.test.js
@@ -45,3 +45,49 @@ const htmlIndent2spaces = `<ul>
 test('convert basic tree to HTML with 2 spaces indentation', () => {
     expect(convertJsonToHtml(tree, 2)).toBe(htmlIndent2spaces);
 })
+
+// Extra sample
+// Input:
+const treeExtra = {
+    tag: "ul",
+    children: [
+        {
+            text: "Primer elemento"
+        },
+        {
+            text: "Cáspita, otro elemento",
+            children: [
+                {
+                    tag: "ul",
+                    children: [
+                        {
+                            text: 'Primer heredero del cáspita'
+                        },
+                        {
+                            text: 'Último heredero del cáspita'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            text: "El último"
+        }
+    ]
+}
+// Output:
+const htmlExtra = `<ul>
+    <li>Primer elemento</li>
+    <li>
+        Cáspita, otro elemento
+        <ul>
+            <li>Primer heredero del cáspita</li>
+            <li>Último heredero del cáspita</li>
+        </ul>
+    </li>
+    <li>El último</li>
+</ul>`
+
+test('convert tree with subtree to HTML with 4 spaces indentation', () => {
+    expect(convertJsonToHtml(treeExtra, 4)).toBe(htmlExtra);
+})
