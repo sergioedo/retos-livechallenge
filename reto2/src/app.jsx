9599b99fc1b1adafd9ceccd52e076417d95e8d37
Sergio Edo <sergio.edo@gmail.com>
2021-02-14 11:27:35 +0100

    reto 2 funcionando

--- reto2/src/app.jsx
+++ reto2/src/app.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
+const App = () => {
+    const [counter, setCounter] = useState(20)
+    const [intervalID, setIntervalID] = useState()
+
+    const countDown = () => {
+        setCounter(counter => {
+            if (counter > 0) {
+                return counter - 1
+            }
+            return counter
+        })
+    }
+
+    useEffect(() => {
+        const timer = window.setInterval(countDown, 1000)  //activamos el timer para descontar 1, cada 1s
+        setIntervalID(timer) //activamos el timer, cada 1s
+
+        return () => {
+            clearInterval(intervalID) //liberamos el timer al desmontar el componente
+        }
+    }, [])
+    return (
+        <>
+            <h1>Malandriner CountDown: {counter}s</h1>
+        </>
+    )
+}
+
 ReactDOM.render(
-    <h1>Hello malandriners!!!</h1>,
+    <App />,
     document.getElementById('app')
 )
